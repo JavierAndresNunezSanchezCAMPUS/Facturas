7fd31a928d058d9f967a5e158eab80d973f9f9a2
JavierAndresNunezSanchezCAMPUS <javierandresn12@gmail.com>
2023-05-26 11:30:35 -0500

    INSERT DELETE Y UPDATE

---
+++ db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE FacturasJav
+
+CREATE TABLE javturas(
+    id INT primary key AUTO_INCREMENT,
+    nombre VARCHAR (50) NOT NULL,
+    descripcion VARCHAR (50),
+    imagen VARCHAR (30)
+);
